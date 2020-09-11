680b1d6bbfb6c2ffb974d37f9d7f837d3c5079d2
Nathan Ray <71066202+nathan-ray@users.noreply.github.com>
2020-09-11 13:42:49 +0700

    perubahan Ray

--- latian git bareng2/src/latian/git/bareng2/LatianGitBareng2.java
+++ latian git bareng2/src/latian/git/bareng2/LatianGitBareng2.java
@@ -16,7 +16,7 @@ public class LatianGitBareng2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("perubahan 1");
+        System.out.println("perubahan RAYYYYYY");
     }
     
 }
