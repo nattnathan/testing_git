f5740742d3fe9987135d9357e0daac26ebb8a1d6
Jonathan purnama halim <46183646+nattnathan@users.noreply.github.com>
2020-09-11 14:04:59 +0700

    Revert "commit push branch ray"
    This reverts commit cfaa9887e7925e2a85a5c0531addfa34583dbf9e.

--- latian git bareng2/src/latian/git/bareng2/LatianGitBareng2.java
+++ latian git bareng2/src/latian/git/bareng2/LatianGitBareng2.java
@@ -17,8 +17,6 @@ public class LatianGitBareng2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("perubahan RAYYYYYY");
-        
-        System.out.println("halooo ini di cabang ray");
     }
     
 }
